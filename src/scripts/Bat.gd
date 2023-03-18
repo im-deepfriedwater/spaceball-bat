f293b07824ffc6deb93c10f44728d5507979d403
Justin Torres <deepfriedwater1@gmail.com>
2023-03-18 00:51:43 -0700

    add input mapping for wasd to ui direction, added physics code for flappy type movement, restructure bat to be a kinematic body at root node

--- src/scripts/Bat.gd
+++ src/scripts/Bat.gd
@@ -1,5 +1,4 @@
-extends Node2D
-
+extends KinematicBody2D
 
 enum MovementType {
 	Linear,
@@ -8,34 +7,76 @@ enum MovementType {
 
 var current_movement_type = MovementType.Linear
 
+# Probably going to break this out into a state machine
+# to break this out a little more
 # Flappy Movement Region
-export var flappy_impulse: float =  4
-export var flappy_gravity: float = -9.8
-export var flappy_buffer_in_seconds: float = 0.25
-export var y_velocity : float = 0 
-
+export var flappy_vertical_max_speed: float =  55
+export var flappy_gravity: float = 105
+export var flappy_input_buffer_in_seconds: float = 0.25
+export var flappy_horizontal_impulse: float = 35
+export var flappy_horizontal_max_speed: float = 80
+export var flappy_friction: float = 55
+export var flappy_is_floaty_horizontal: bool = true
+onready var flappy_input_buffer_timer := $FlappyInputBufferTimer
+var flappy_is_input_buffering : bool = false
 # Flappy Movement End Region
+
+var bat_velocity := Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	flappy_input_buffer_timer.wait_time = flappy_input_buffer_in_seconds
+	current_movement_type = MovementType.Flappy
 	
 func _process(delta: float):
+	var input_vector := Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
 	match current_movement_type:
-		
 		MovementType.Linear:
-			handle_linear_movement()
+			handle_linear_movement(delta, input_vector)
 			
 		MovementType.Flappy:
-			handle_flappy_movement(delta)
-		
+			handle_flappy_movement(delta, input_vector)
 	
 # TODO fill in here
-func handle_linear_movement():
+func handle_linear_movement(delta: float, input_vector: Vector2):
 	pass
 
+# TODO
+func handle_flappy_movement(delta: float, input_vector: Vector2):
+	bat_velocity.y += delta * flappy_gravity
 
-# TODO"
-func handle_flappy_movement(delta: float):
-	if (Input.is_action_pressed("ui_accept")):
-		pass
+	if !flappy_is_input_buffering and Input.is_action_just_pressed("ui_accept"):
+		bat_velocity.y = -flappy_vertical_max_speed
+		flappy_is_input_buffering = true
+		flappy_input_buffer_timer.start()
+		
+	if Input.is_action_pressed("ui_left"):
+		bat_velocity.x += -flappy_horizontal_impulse
+	elif Input.is_action_pressed("ui_right"):
+		bat_velocity.x +=  flappy_horizontal_impulse
+
+	bat_velocity.x = bat_velocity.x + delta * flappy_friction if flappy_is_floaty_horizontal else 0
+
+		
+	if bat_velocity.x <= 3 and bat_velocity.x >= 0 \
+		|| bat_velocity.x >= -3 and bat_velocity.x <= 0:
+		bat_velocity.x = 0
+	
+	if bat_velocity.x > 0:
+		bat_velocity.x = clamp(bat_velocity.x, 0, flappy_horizontal_max_speed)
+	elif bat_velocity.x < 0:
+		bat_velocity.x = clamp(bat_velocity.x, -flappy_horizontal_max_speed, 0)
+	
+	move()
+
+
+func move():
+	# in Godot, upward is negative y, which translates to -1 as a normal
+	bat_velocity = move_and_slide(bat_velocity, Vector2(0, -1))
+	
+func _on_InputBufferTimer_timeout():
+	flappy_is_input_buffering = false
